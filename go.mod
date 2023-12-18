a14e59314b2d135141466e4f288869a58d0119ef
joseavilasg <joseavilasg.dev@gmail.com>
2023-12-18 00:09:03 -0500

    Implement new progress bar

--- go.mod
+++ go.mod
@@ -4,7 +4,14 @@ go 1.21
 
 require github.com/schollz/progressbar/v3 v3.13.1
 
-require github.com/gofrs/uuid v4.4.0+incompatible // indirect
+require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/cheggaaa/pb/v3 v3.1.4 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+)
 
 require (
 	github.com/jzelinskie/whirlpool v0.0.0-20201016144138-0675e54bb004 // indirect
--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 
 	"flag"
 
+	"github.com/cheggaaa/pb/v3"
 	"github.com/gofrs/uuid"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/rclone/rclone/fs"
@@ -28,7 +29,6 @@ import (
 	"github.com/rclone/rclone/lib/rest"
 
 	"github.com/joho/godotenv"
-	"github.com/schollz/progressbar/v3"
 )
 
 var Info = log.New(os.Stdout, "\u001b[34mINFO: \u001B[0m", log.LstdFlags|log.Lshortfile)
@@ -267,29 +267,49 @@ func (u *Uploader) uploadFile(filePath string, destDir string) error {
 		encryptFile = uploadFile.Parts[0].Encrypted
 	}
 
-	bar := progressbar.NewOptions64(fileSize,
-		progressbar.OptionShowCount(),
-		progressbar.OptionSetWriter(os.Stderr),
-		progressbar.OptionEnableColorCodes(true),
-		progressbar.OptionShowBytes(true),
-		progressbar.OptionSetWidth(10),
-		progressbar.OptionThrottle(65*time.Millisecond),
-		progressbar.OptionSetDescription(fileName),
-		progressbar.OptionSetTheme(progressbar.Theme{
-			Saucer:        "[green]=[reset]",
-			SaucerHead:    "[green]>[reset]",
-			SaucerPadding: " ",
-			BarStart:      "[",
-			BarEnd:        "]",
-		}),
-		progressbar.OptionFullWidth(),
-		progressbar.OptionSetRenderBlankState(true))
+	// bar := progressbar.NewOptions64(fileSize,
+	// 	progressbar.OptionShowCount(),
+	// 	progressbar.OptionSetWriter(os.Stderr),
+	// 	progressbar.OptionEnableColorCodes(true),
+	// 	progressbar.OptionShowBytes(true),
+	// 	progressbar.OptionSetWidth(10),
+	// 	progressbar.OptionThrottle(65*time.Millisecond),
+	// 	progressbar.OptionSetDescription(fileName),
+	// 	progressbar.OptionSetTheme(progressbar.Theme{
+	// 		Saucer:        "[green]=[reset]",
+	// 		SaucerHead:    "[green]>[reset]",
+	// 		SaucerPadding: " ",
+	// 		BarStart:      "[",
+	// 		BarEnd:        "]",
+	// 	}),
+	// 	progressbar.OptionFullWidth(),
+	// 	progressbar.OptionSetRenderBlankState(true))
+
+	const maxFileNameLength = 55
+
+	if len(fileName) > maxFileNameLength {
+		half := maxFileNameLength / 2
+		fileName = fileName[:half-2] + "..." + fileName[len(fileName)-half+1:]
+	}
+
+	tmpl := `{{string . "fileName"}}  {{percent . }} {{bar . }} ({{counters . }}, {{speed . }}) [{{etime . "%s"}}:{{rtime . "%s"}}]`
+	progressBar := pb.ProgressBarTemplate(tmpl).Start64(fileSize).
+		SetRefreshRate(time.Second).
+		SetWriter(os.Stderr).
+		Set(pb.Bytes, true).
+		Set(pb.SIBytesPrefix, true).
+		Set("fileName", fileName)
+
+	if err := progressBar.Err(); err != nil {
+		return err
+	}
 
 	go func() {
 		wg.Wait()
 		close(uploadedParts)
-		bar.Finish()
-		bar.Close()
+		progressBar.Finish()
+		// bar.Finish()
+		// bar.Close()
 	}()
 
 	var partName string
@@ -318,7 +338,8 @@ func (u *Uploader) uploadFile(filePath string, destDir string) error {
 			defer file.Close()
 			if existing, ok := existingParts[int(partNumber)+1]; ok {
 				uploadedParts <- existing
-				bar.Add64(existing.Size)
+				// bar.Add64(existing.Size)
+				progressBar.Add64(existing.Size)
 				return
 			}
 
@@ -329,12 +350,13 @@ func (u *Uploader) uploadFile(filePath string, destDir string) error {
 				return
 			}
 
-			pr := &ProgressReader{file, func(r int64) {
-				bar.Add64(r)
-			}}
+			// pr := &ProgressReader{file, func(r int64) {
+			// 	bar.Add64(r)
+			// }}
+			barReader := progressBar.NewProxyReader(file)
 
 			contentLength := end - start
-			reader := io.LimitReader(pr, contentLength)
+			reader := io.LimitReader(barReader, contentLength)
 
 			if u.randomisePart {
 				u1, _ := uuid.NewV4()
@@ -425,7 +447,6 @@ func (u *Uploader) uploadFile(filePath string, destDir string) error {
 
 	return nil
 }
-
 func (u *Uploader) createRemoteDir(path string) error {
 	opts := rest.Opts{
 		Method: "POST",
